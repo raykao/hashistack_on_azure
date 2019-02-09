55600dfaf7c3451aa43d796060fe27017b4a278a
Raymond Kao <rakao@microsoft.com>
2019-02-08 23:34:33 -0500

    adding a systemd script for nomad startup

--- packer/shell_provisioners/install_nomad.sh
+++ packer/shell_provisioners/install_nomad.sh
@@ -23,3 +23,31 @@ chown --recursive nomad:nomad /opt/nomad
 
 touch /etc/systemd/system/nomad.service
 
+cat >/etc/systemd/system/nomad.service <<EOF
+# Modified from ref: https://github.com/hashicorp/nomad/blob/master/dist/systemd/nomad.service
+[Unit]
+Description=Nomad
+Documentation=https://nomadproject.io/docs/
+Wants=network-online.target
+After=network-online.target
+
+# If you are running Consul, please uncomment following Wants/After configs.
+# Assuming your Consul service unit name is "consul"
+#Wants=consul.service
+#After=consul.service
+
+[Service]
+KillMode=process
+KillSignal=SIGINT
+ExecStart=/usr/local/bin/nomad agent -config /etc/nomad.d
+ExecReload=/bin/kill -HUP $MAINPID
+Restart=on-failure
+RestartSec=2
+StartLimitBurst=3
+StartLimitIntervalSec=10
+LimitNOFILE=65536
+TasksMax=infinity
+
+[Install]
+WantedBy=multi-user.target
+EOF
