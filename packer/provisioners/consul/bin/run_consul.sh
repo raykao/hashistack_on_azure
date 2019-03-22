1b6683fd2f5b17231ea52cd18e172b12b8985a87
Raymond Kao <rakao@microsoft.com>
2019-03-22 01:36:15 -0400

    allowing server config to happen at provisioning time

--- packer/provisioners/consul/bin/run_consul.sh
+++ packer/provisioners/consul/bin/run_consul.sh
@@ -1,12 +1,13 @@
-## The Env Vars below are passed in from super script
-## AZURE_SUBSCRIPTION_ID
-## CONSUL_VMSS_RG
-## CONSUL_VMSS_NAME
-## CONSUL_GOSSIP_ENCRYPT_KEY
+## The Env Vars
+# AZURE_SUBSCRIPTION_ID
+# CONSUL_VMSS_RG
+# CONSUL_VMSS_NAME
+# CONSUL_GOSSIP_ENCRYPT_KEY
+## are prepended at provisioning time
 
-export AZURE_MSI_ENDPOINT="http://169.254.169.254/metadata/identity"
-export AZURE_MSI_OAUTH=$(curl -H "Metadata:true" $AZURE_MSI_ENDPOINT"/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F")
-export AZURE_MSI_JWT=$(echo $AZURE_MSI_OAUTH | jq -r '.access_token')
+AZURE_MSI_ENDPOINT="http://169.254.169.254/metadata/identity"
+AZURE_MSI_OAUTH=$(curl -H "Metadata:true" $AZURE_MSI_ENDPOINT"/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F")
+AZURE_MSI_JWT=$(echo $AZURE_MSI_OAUTH | jq -r '.access_token')
 
 function getVMSSprivateIPAddresses () {
   consulRetryJoin=""
@@ -22,6 +23,4 @@ function getVMSSprivateIPAddresses () {
 
 consul agent \
   -config-dir="/opt/consul/config" \
-  -data-dir="/opt/consul/data" \
-  -encrypt=$CONSUL_GOSSIP_ENCRYPT_KEY \
   $(getVMSSprivateIPAddresses)
