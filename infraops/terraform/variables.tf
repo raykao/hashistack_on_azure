659b6e95fec105040788ef9c5db46b43d477a090
Raymond Kao <rakao@microsoft.com>
2019-04-10 09:47:45 -0400

    updated consul and nomad versions for base image

--- infraops/terraform/variables.tf
+++ infraops/terraform/variables.tf
@@ -1,8 +1,10 @@
 variable "CLUSTER_ENVIRONMENT" {
+  description = "Optional - defaults to 'test'.  Used to tag all the services with the deployment environment type."
   default = "test"
 }
 
 variable "AZURE_RESOURCE_GROUP_NAME" {
+  description = "Optional - the resource group for shared resources for the cluster.  Example: Azure VNET"
   default = "hashicluster"
 }
 
@@ -11,14 +13,17 @@ variable "AZURE_DC_LOCATION" {
 }
 
 variable "CONSUL_VMSS_NAME" {
+  description = "Optional - the name of the Consul VMSS cluster.  Defaults to 'consul-servers'"
   default = "consul-servers"
 }
 
 variable "CONSUL_VMSS_RG" {
+  description = "Optional - the resource group where the Consul Server Cluster will deployed into. Defaults to 'consul-servers'"
   default = "consul-servers"
 }
 
 variable "CONSUL_MASTER_TOKEN" {
+  description = "Optional - The Consul Master (Management) ACL Token.  It will be computed and then seeded into the Server Configs if not supplied"
   default = ""
 }
 
@@ -33,7 +38,7 @@ variable "CONSUL_SERVER_CLUSTER_VM_SIZE" {
 }
 
 variable "HASHI_MANAGED_VM_IMAGE_NAME" {
-  
+  description = "Required - The customized image that will be used to deploy the cluster"
 }
 
 variable "ADMIN_NAME" {
@@ -45,6 +50,7 @@ variable "SSH_PUBLIC_KEY" {
 }
 
 variable "CONSUL_ENCRYPT_KEY" {
+  description = "Optional - the Consul Gossip Encryption Key."
   default = ""
 }
 
